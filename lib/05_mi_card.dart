de99f9ace4955bb18a2268f4f411354e7f357ff7
Andrei Popovici <me@andreipopovici.co.uk>
2020-01-13 08:06:35 +0000

    WIP MiCard

---
+++ lib/05_mi_card.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
